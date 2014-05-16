5bc4a41c269234fdb53b88ec7e0034d661afbf32
dedyk <dedykjp@gmail.com>
2014-05-16 23:06:29 +0200

    update

--- src/main/java/pl/idedyk/japanese/dictionary/api/dictionary/dto/FindKanjiResult.java
+++ src/main/java/pl/idedyk/japanese/dictionary/api/dictionary/dto/FindKanjiResult.java
@@ -1,10 +1,13 @@
 package pl.idedyk.japanese.dictionary.api.dictionary.dto;
 
+import java.io.Serializable;
 import java.util.List;
 
 import pl.idedyk.japanese.dictionary.api.dto.KanjiEntry;
 
-public class FindKanjiResult {
+public class FindKanjiResult implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	public List<KanjiEntry> result;
 	
