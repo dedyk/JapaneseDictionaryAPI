3c91d95a64a369bccac9b0e3cabd5c3744aea7bd
dedyk <dedykjp@gmail.com>
2014-08-29 08:52:17 +0200

    update

--- src/main/java/pl/idedyk/japanese/dictionary/api/dto/KanjivgEntry.java
+++ src/main/java/pl/idedyk/japanese/dictionary/api/dto/KanjivgEntry.java
@@ -7,6 +7,8 @@ public class KanjivgEntry implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
+	private String kanji;
+	
 	private List<String> strokePaths;
 	
 	public KanjivgEntry() {
@@ -22,5 +24,13 @@ public class KanjivgEntry implements Serializable {
 
 	public void setStrokePaths(List<String> strokePaths) {
 		this.strokePaths = strokePaths;
+	}
+
+	public String getKanji() {
+		return kanji;
+	}
+
+	public void setKanji(String kanji) {
+		this.kanji = kanji;
 	}
 }
