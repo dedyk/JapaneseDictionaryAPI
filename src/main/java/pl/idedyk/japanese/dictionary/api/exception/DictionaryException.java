dc442d40fad9cf46de619976fb99ad7018ad0d30
dedyk <dedykjp@gmail.com>
2018-02-18 12:50:38 +0100

    update

--- src/main/java/pl/idedyk/japanese/dictionary/api/exception/DictionaryException.java
+++ src/main/java/pl/idedyk/japanese/dictionary/api/exception/DictionaryException.java
@@ -3,6 +3,10 @@ package pl.idedyk.japanese.dictionary.api.exception;
 public class DictionaryException extends Exception {
 
 	private static final long serialVersionUID = 1L;
+	
+	public DictionaryException(Exception e) {
+		super(e);
+	}
 
 	public DictionaryException(String detailMessage) {
 		super(detailMessage);
