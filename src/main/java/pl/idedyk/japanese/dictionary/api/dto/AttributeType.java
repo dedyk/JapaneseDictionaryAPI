89967d93403392971466f7ae06564c03ec152f7f
Fryderyk Mazurek <fryderyk.mazurek@gmail.com>
2022-07-21 08:33:45 +0200

    update

--- src/main/java/pl/idedyk/japanese/dictionary/api/dto/AttributeType.java
+++ src/main/java/pl/idedyk/japanese/dictionary/api/dto/AttributeType.java
@@ -19,7 +19,7 @@ public enum AttributeType {
 	KANJI_ALONE("Zwykle pisany przy użyciu kanji", true),
 	KANA_ALONE("zwykle pisany przy użyciu kana", true),
 	
-	ARCHAIC("archaizm", true),
+	ARCHAIC("archaiczny", true),
 	
 	OBSCURE("słowo mało znane, zapomniane", true),
 	
