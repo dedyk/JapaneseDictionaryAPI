02088e33df396a61d96e3b1a3a74fcb15ed82022
Fryderyk Mazurek <fryderyk.mazurek@gmail.com>
2023-04-09 14:05:48 +0200

    update

--- src/main/java/pl/idedyk/japanese/dictionary2/api/helper/Dictionary2HelperCommon.java
+++ src/main/java/pl/idedyk/japanese/dictionary2/api/helper/Dictionary2HelperCommon.java
@@ -1199,6 +1199,9 @@ public class Dictionary2HelperCommon {
 			case YODAN_VERB_WITH_GU_ENDING_ARCHAIC:
 				result.add("czasownik yodan kończący się na gu (archaiczny)"); break;
 				
+			case YODAN_VERB_WITH_TSU_ENDING_ARCHAIC:
+				result.add("czasownik yodan kończący się na tsu (archaiczny)"); break;
+				
 			case SHIKU_ADJECTIVE_ARCHAIC:
 				result.add("przymiotnik shiku (archaiczny)"); break;
 				
