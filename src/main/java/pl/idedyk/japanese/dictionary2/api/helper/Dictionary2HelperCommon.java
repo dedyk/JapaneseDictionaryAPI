2c793f57b22671269ff62b40a7f27292653f8cad
Fryderyk Mazurek <fryderyk.mazurek@gmail.com>
2023-12-26 16:39:06 +0100

    update

--- src/main/java/pl/idedyk/japanese/dictionary2/api/helper/Dictionary2HelperCommon.java
+++ src/main/java/pl/idedyk/japanese/dictionary2/api/helper/Dictionary2HelperCommon.java
@@ -1288,7 +1288,10 @@ public class Dictionary2HelperCommon {
 				
 			case NIDAN_VERB_UPPER_CLASS_WITH_GU_ENDING_ARCHAIC:
 				result.add("czasownik nidan (wyższa klasa) kończący się na gu (archaiczny)"); break;				
-				
+
+			case NIDAN_VERB_UPPER_CLASS_WITH_KU_ENDING_ARCHAIC:
+				result.add("czasownik nidan (wyższa klasa) kończący się na ku (archaiczny)"); break;				
+
 			case YODAN_VERB_WITH_KU_ENDING_ARCHAIC:
 				result.add("czasownik yodan kończący się na ku (archaiczny)"); break;
 
