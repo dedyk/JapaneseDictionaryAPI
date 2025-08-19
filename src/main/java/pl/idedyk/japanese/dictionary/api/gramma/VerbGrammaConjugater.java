3744f411a36057766bf079bcbf0ee87eeba79600
Fryderyk Mazurek <fryderyk.mazurek@gmail.com>
2025-08-19 16:21:57 +0200

    Dictionary 2

--- src/main/java/pl/idedyk/japanese/dictionary/api/gramma/VerbGrammaConjugater.java
+++ src/main/java/pl/idedyk/japanese/dictionary/api/gramma/VerbGrammaConjugater.java
@@ -163,6 +163,8 @@ public class VerbGrammaConjugater {
 
 	public static List<GrammaFormConjugateGroupTypeElements> makeAll(KeigoHelper keigoHelper, GrammaFormConjugateRequest grammaFormConjugateRequest, Map<GrammaFormConjugateResultType, GrammaFormConjugateResult> grammaFormCache, boolean addVirtual) {
 
+		// FM_FIXME: zrobic to inaczej
+		
 		// validate DictionaryEntry
 		validateDictionaryEntry(grammaFormConjugateRequest);
 		
