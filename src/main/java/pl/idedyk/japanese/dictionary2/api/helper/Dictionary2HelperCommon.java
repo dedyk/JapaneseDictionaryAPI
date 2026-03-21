eb2045622a7d2ec3e9f6881d1e81396edb4ae105
Fryderyk Mazurek <fryderyk.mazurek@gmail.com>
2026-03-21 13:36:08 +0100

    update

--- src/main/java/pl/idedyk/japanese/dictionary2/api/helper/Dictionary2HelperCommon.java
+++ src/main/java/pl/idedyk/japanese/dictionary2/api/helper/Dictionary2HelperCommon.java
@@ -1377,7 +1377,7 @@ public class Dictionary2HelperCommon {
 				result.add("rzeczownik, używany jako przyrostek"); break;
 				
 			case ADJECTIVAL_NOUNS_OR_QUASI_ADJECTIVES_KEIYODOSHI:
-				result.add("rzeczownik przymiotnikowy lub quasi-przymiotnik (keiyodoshi)"); break;
+				result.add("rzeczownik przymiotnikowy lub quasi-przymiotnik (keiyou doshi)"); break;
 				
 			case NOUN_OR_PARTICIPLE_WHICH_TAKES_THE_AUX_VERB_SURU:
 				result.add("rzeczownik lub imiesłów, który przyjmuje czasownik posiłkowy suru"); break;
